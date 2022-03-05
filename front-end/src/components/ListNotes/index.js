edefde7889144920fc768e49243bd232099f5547
João Xavier <jvxavier@inf.ufsm.br>
2022-03-04 23:19:58 -0300

    Created Components

---
+++ front-end/src/components/ListNotes/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ListNotes = styled.aside`
+    background-color: gray;
+    min-width: 300px;
+    max-width: 500px;
+    max-height: 90%;
+`
+
+export default ListNotes;
