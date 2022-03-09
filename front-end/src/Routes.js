1810d395a8145ef59ee3b3cf2b4feabdf4a9fcc0
João Xavier <jvxavier@inf.ufsm.br>
2022-03-09 11:47:51 -0300

    add private routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,22 +2,39 @@ import React from "react";
 import {
     BrowserRouter,
     Routes as Switch,
-    Route
+    Route,
+    Navigate
 } from "react-router-dom";
+
+//Rotas
 import Home from "./pages/home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import App from "./pages/app";
 
+const TOKEN = "@Token-Xnote";
+
 const Routes = () => {
 
-    return(
+    const PrivateRoute = ({children}) => {
+
+        const token = localStorage.getItem(TOKEN);
+
+        return token ? (
+            children
+        ) : (
+            <Navigate to="/login" />
+        )
+
+    }
+
+    return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/app" element={<App />}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/app" element={<PrivateRoute> <App /> </PrivateRoute>} />
             </Switch>
         </BrowserRouter>
     )
